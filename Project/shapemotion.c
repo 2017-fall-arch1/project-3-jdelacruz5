fa31291e6582a62a247dc0e3b68f00a9eec75a8e
student <student@localhost.localdomain>
2017-11-28 17:48:35 -0700

    updated score to change on bounce

--- Project/shapemotion.c
+++ Project/shapemotion.c
@@ -16,51 +16,50 @@
 
 #define GREEN_LED BIT6
 
+int lscore = 0;
+int rscore = 0;
+char score[3];
+
 
 AbRect rect10 = {abRectGetBounds, abRectCheck, {5,20}}; /**< 10x10 rectangle */
 AbRect rect11 = {abRectGetBounds, abRectCheck, {5,20}};
 
 
 AbRectOutline fieldOutline = {	/* playing field */
-  abRectOutlineGetBounds, abRectOutlineCheck,   
-  {(screenWidth/2)-.5, (screenHeight/2)-.5}
+		abRectOutlineGetBounds, abRectOutlineCheck,   
+		{(screenWidth/2)-.5, (screenHeight/2)-.5}
 };
 
 Layer fieldLayer = {		//playing field as a layer 
-  (AbShape *) &fieldOutline,
-  {screenWidth/2, screenHeight/2},//< center
-  {0,0}, {0,0},				    // last & next pos 
-  COLOR_WHITE,
-  0
+		(AbShape *) &fieldOutline,
+		{screenWidth/2, screenHeight/2},//< center
+		{0,0}, {0,0},				    // last & next pos 
+		COLOR_WHITE,
+		0
 };
 
-/*Layer score = {
-  drawString5x7(50,10, "switches:", COLOR_GREEN, COLOR_BLUE);
-  &fieldLayer
-};*/
-
 Layer layer2 = {
-  (AbShape *)&rect11,
-  {(screenWidth/10)-10, (screenHeight/2)}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_BLUE,
-  &fieldLayer
+		(AbShape *)&rect11,
+		{(screenWidth/10)-10, (screenHeight/2)}, /**< bit below & right of center */
+		{0,0}, {0,0},				    /* last & next pos */
+		COLOR_BLUE,
+		&fieldLayer
 };
 
 Layer layer1 = {		//< Layer with a red square
-  (AbShape *)&rect10,
-  {screenWidth-5, (screenHeight/2)}, //< center
-  {0,0}, {0,0},				    // last & next pos
-  COLOR_ORANGE,
-  &layer2,
+		(AbShape *)&rect10,
+		{screenWidth-5, (screenHeight/2)}, //< center
+		{0,0}, {0,0},				    // last & next pos
+		COLOR_ORANGE,
+		&layer2,
 };
 
 Layer layer0 = {		//< Layer with an orange circle 
-  (AbShape *)&circle4,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, //< bit below & right of center 
-  {0,0}, {0,0},				    //last & next pos 
-  COLOR_BLACK,
-  &layer1,
+		(AbShape *)&circle4,
+		{(screenWidth/2), (screenHeight/2)}, //< bit below & right of center 
+		{0,0}, {0,0},				    //last & next pos 
+		COLOR_BLACK,
+		&layer1,
 };
 
 /** Moving Layer
@@ -68,52 +67,52 @@ Layer layer0 = {		//< Layer with an orange circle
  *  Velocity represents one iteration of change (direction & magnitude)
  */
 typedef struct MovLayer_s {
-  Layer *layer;
-  Vec2 velocity;
-  struct MovLayer_s *next;
+	Layer *layer;
+	Vec2 velocity;
+	struct MovLayer_s *next;
 } MovLayer;
 
 /* initial value of {0,0} will be overwritten */
 //MovLayer ml1 = { &layer1, {1,2}, 0};//&m13; 
-MovLayer ml0 = { &layer0, {2,1}, 0 };
+MovLayer ml0 = { &layer0, {3,3}, 0 };
 //MovLayer ml3 = { &layer3, {1,1}, 0 }; /**< not all layers move */
 
 
 void movLayerDraw(MovLayer *movLayers, Layer *layers)
 {
-  int row, col;
-  MovLayer *movLayer;
+	int row, col;
+	MovLayer *movLayer;
 
-  and_sr(~8);			/**< disable interrupts (GIE off) */
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Layer *l = movLayer->layer;
-    l->posLast = l->pos;
-    l->pos = l->posNext;
-  }
-  or_sr(8);			/**< disable interrupts (GIE on) */
+	and_sr(~8);			/**< disable interrupts (GIE off) */
+	for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+		Layer *l = movLayer->layer;
+		l->posLast = l->pos;
+		l->pos = l->posNext;
+	}
+	or_sr(8);			/**< disable interrupts (GIE on) */
 
 
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Region bounds;
-    layerGetBounds(movLayer->layer, &bounds);
-    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
-		bounds.botRight.axes[0], bounds.botRight.axes[1]);
-    for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
-      for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
-	Vec2 pixelPos = {col, row};
-	u_int color = bgColor;
-	Layer *probeLayer;
-	for (probeLayer = layers; probeLayer; 
-	     probeLayer = probeLayer->next) { /* probe all layers, in order */
-	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
-	    color = probeLayer->color;
-	    break; 
-	  } /* if probe check */
-	} // for checking all layers at col, row
-	lcd_writeColor(color); 
-      } // for col
-    } // for row
-  } // for moving layer being updated
+	for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+		Region bounds;
+		layerGetBounds(movLayer->layer, &bounds);
+		lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
+				bounds.botRight.axes[0], bounds.botRight.axes[1]);
+		for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
+			for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
+				Vec2 pixelPos = {col, row};
+				u_int color = bgColor;
+				Layer *probeLayer;
+				for (probeLayer = layers; probeLayer; 
+						probeLayer = probeLayer->next) { /* probe all layers, in order */
+					if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
+						color = probeLayer->color;
+						break; 
+					} /* if probe check */
+				} // for checking all layers at col, row
+				lcd_writeColor(color); 
+			} // for col
+		} // for row
+	} // for moving layer being updated
 }	  
 
 
@@ -127,21 +126,43 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
  */
 void mlAdvance(MovLayer *ml, Region *fence)
 {
-  Vec2 newPos;
-  u_char axis;
-  Region shapeBoundary;
-  for (; ml; ml = ml->next) {
-    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
-    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
-    for (axis = 0; axis < 2; axis ++) {
-      if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
+
+	score[1] = '|';
+	Vec2 newPos;
+	u_char axis;
+	Region shapeBoundary;
+    
+	for (; ml; ml = ml->next) {
+		
+		vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+		abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+
+		for (axis = 0; axis < 2; axis ++) {
+			/*   if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
 	  (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
 	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
 	newPos.axes[axis] += (2*velocity);
-      }	/**< if outside of fence */
-    } /**< for axis */
-    ml->layer->posNext = newPos;
-  } /**< for ml */
+      }	//< if outside of fence */
+            
+			if (shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) {
+				int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+				newPos.axes[axis] += (2*velocity);
+				lscore += 1;
+				score[0] = '0' + lscore;
+			} /**< for axis */
+
+			if (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) {
+				int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+				newPos.axes[axis] += (2*velocity);
+				rscore += 1;
+				score[2] = '0' + rscore;
+			}
+
+			ml->layer->posNext = newPos;
+
+		} /**< for ml */
+	}
+	drawString5x7(55,5, score , COLOR_BLACK, COLOR_WHITE);
 }
 
 
@@ -156,52 +177,51 @@ Region fieldFence;		/**< fence around playing field  */
  */
 void main()
 {
-  P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
-  P1OUT |= GREEN_LED;
 
-  configureClocks();
-  lcd_init();
-  shapeInit();
-  p2sw_init(1);
+	P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
+	P1OUT |= GREEN_LED;
 
-  shapeInit();
+	configureClocks();
+	lcd_init();
+	shapeInit();
+	p2sw_init(1);
 
-  layerInit(&layer0);
-  layerDraw(&layer0);
+	shapeInit();
 
-  
-  layerGetBounds(&fieldLayer, &fieldFence);
-  drawString5x7(50,5, "0 | 0", COLOR_BLACK, COLOR_WHITE);
-
-  enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
+	layerInit(&layer0);
+	layerDraw(&layer0);
 
 
-  for(;;) { 
-    while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
-      P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
-      or_sr(0x10);	      /**< CPU OFF */
-    }
-    
-    drawString5x7(50,5, "0 | 0", COLOR_BLACK, COLOR_WHITE);
+	layerGetBounds(&fieldLayer, &fieldFence);
+	drawString5x7(55,5, "0|0", COLOR_BLACK, COLOR_WHITE); //update to use variables
 
-    P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
-    redrawScreen = 0;
-    movLayerDraw(&ml0, &layer0);
-  }
+	enableWDTInterrupts();      /**< enable periodic interrupt */
+	or_sr(0x8);	              /**< GIE (enable interrupts) */
+
+
+	for(;;) { 
+		while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
+			P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
+			or_sr(0x10);	      /**< CPU OFF */
+		}
+
+		P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
+		redrawScreen = 0;
+		movLayerDraw(&ml0, &layer0);
+	}
 }
 
 /** Watchdog timer interrupt handler. 15 interrupts/sec */
 void wdt_c_handler()
 {
-  static short count = 0;
-  P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
-  count ++;
-  if (count == 15) {
-    mlAdvance(&ml0, &fieldFence);
-    if (p2sw_read())
-      redrawScreen = 1;
-    count = 0;
-  } 
-  P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
+	static short count = 0;
+	P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
+	count ++;
+	if (count == 15) {
+		mlAdvance(&ml0, &fieldFence);
+		if (p2sw_read())
+			redrawScreen = 1;
+		count = 0;
+	} 
+	P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
 }
